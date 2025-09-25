5abedde6b6b89e6e160d10870929b51f85a4641d
harryfafa <g96249624@gmail.com>
2025-09-26 03:02:22 +0800

    remove Extra code

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,18 +6,15 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function Home() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
     </SafeAreaProvider>
   );
